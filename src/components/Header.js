c1b57e8c376b1ee0f124ef9166d6e67921c90a22
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-06-17 10:08:17 +0200

    fix song

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,14 @@ import song from "../audio/song.mp3";
 const Header = () => {
   return (
     <div className="header">
-      <audio id="song" type="audio/mp3" controls autoPlay className="song">
+      <audio
+        id="song"
+        type="audio/mp3"
+        controls
+        autoPlay
+        className="song"
+        hidden
+      >
         {" "}
         <source src={song} />
       </audio>
